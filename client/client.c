77efcebb6bf1b8a444987844cd170b98eb0bdc1e
NotableDeveloper <castlereal@naver.com>
2021-05-18 22:31:08 +0900

    Init project

--- client/client.c
+++ client/client.c
@@ -1,5 +1,81 @@
-" Press ? for help
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
-.. (up a dir)
-</client/
+void err_proc();
+int main(int argc, char** argv)
+{
+	int clntSd;
+	struct sockaddr_in clntAddr;
+	int clntAddrLen, readLen, recvByte, maxBuff;
+	char wBuff[BUFSIZ];
+	char rBuff[BUFSIZ];
 
+	if(argc != 3)
+		printf("Usage: %s [IP Address] [Port]\n", argv[0]);
+
+	//클라이언트 소켓 생성. (IPv4, TCP를 사용함.)
+	clntSd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(clntSd == -1)
+		err_proc();
+
+	printf("==== client program =====\n");
+
+	memset(&clntAddr, 0, sizeof(clntAddr));
+	/*
+		클라이언트 주소 초기화.
+		IPv4를 사용하고, IP주소와 포트번호는 매개인자를 사용함.
+	*/
+	clntAddr.sin_family = AF_INET;
+	clntAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	clntAddr.sin_port = htons(atoi(argv[2]));
+
+	//서버와 연결을 시도함. 실패 시 예외처리.
+	if(connect(clntSd, (struct sockaddr *) &clntAddr,
+			    sizeof(clntAddr)) == -1)
+	{
+		close(clntSd);
+		err_proc();
+	}
+
+	//서버와 연결되고 난 이후
+	while(1)
+	{
+		//키보드 입력을 받아 메모리 버퍼인 wBuff에 저장.
+		fgets(wBuff,BUFSIZ-1,stdin);
+		readLen = strlen(wBuff);
+
+		if(readLen < 2)
+			continue;
+
+		write(clntSd,wBuff,readLen-1);
+		recvByte = 0;
+		maxBuff = BUFSIZ-1;
+
+		do{	//wBuff에 쓴 만큼, 서버로부터 데이터를 기다렸다가 출력.
+			recvByte += read(clntSd,rBuff,maxBuff);
+			maxBuff -= recvByte;
+		}while(recvByte < (readLen-1));
+		rBuff[recvByte] = '\0';
+		printf("Server: %s\n", rBuff);
+		wBuff[readLen-1]='\0';
+		//입력이 END 였다면, 반복문 탈출.
+		if(!strcmp(wBuff,"END"))
+			break;
+	}
+	printf("END ^^\n");
+	close(clntSd);
+
+	return 0;
+}
+
+void err_proc()
+{
+	fprintf(stderr,"Error: %s\n", strerror(errno));
+	exit(errno);
+}
--- server/server.c
+++ server/server.c
@@ -1,5 +1,107 @@
-" Press ? for help
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
-.. (up a dir)
-</server/
+int main(int argc, char** argv)
+{
+	int listenSd, connectSd;
+	struct sockaddr_in srvAddr, clntAddr;
+	int clntAddrLen, readLen, strLen;
+	char rBuff[BUFSIZ];
+	int maxFd = 0;
+	fd_set defaultFds, rFds;
+	int res, i;
 
+	if(argc != 2)
+	{
+		printf("Usage: %s [Port Number]\n", argv[0]);
+		return -1;
+	}
+
+	printf("Server start...\n");
+	listenSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(listenSd == -1 )
+		printf("socket error");
+
+	memset(&srvAddr, 0, sizeof(srvAddr));
+	//서버 addr 구조체 초기화.
+	srvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	srvAddr.sin_family = AF_INET;
+	srvAddr.sin_port = htons(atoi(argv[1]));
+
+	//소켓과 addr 구조체 바인딩
+	if(bind(listenSd, (struct sockaddr *) &srvAddr,
+				 sizeof(srvAddr)) == -1)
+		printf("bind error\n");
+
+	//5개의 연결 허용.
+	if(listen(listenSd, 5) < 0)
+		printf("listen error\n");
+
+	//fdset의 모든 비트열 초기화.
+	FD_ZERO(&defaultFds);
+	//listenSd 에 해당하는 비트열을 1로 설정.
+	FD_SET(listenSd, &defaultFds);
+	//select 사용을 위해, 생성된 디스크립터의 숫자를 따로 저장.
+	maxFd = listenSd;
+
+	clntAddrLen = sizeof(clntAddr);
+	while(1)
+	{
+		rFds = defaultFds;	//select 호출 전에 fdset을 복사해둠.
+
+		//select으로 서버로 오는 이벤트를 감지.
+		if((res = select(maxFd + 1, &rFds, 0, 0, NULL)) == -1)
+			break;
+
+		for(i=0; i<maxFd+1; i++)
+		{
+			if(FD_ISSET(i, &rFds))
+			{
+				if(i == listenSd) //accept a client
+				{
+					connectSd = accept(listenSd,
+						 (struct sockaddr *) &clntAddr,
+						 &clntAddrLen);
+					if(connectSd == -1)
+					{
+						printf("Accept Error");
+						continue;
+					}
+					printf("A client is connected...\n");
+					//연결된 소켓을 defaultFds에 등록해둠.
+					FD_SET(connectSd, &defaultFds);
+
+					if(maxFd < connectSd){
+						maxFd = connectSd;
+					}
+				}
+				else //연결된 클라이언트의 요청
+				{
+					readLen = read(i, rBuff, sizeof(rBuff)-1);
+					if(readLen <= 0)
+					{
+						printf("A client is disconnected...\n");
+						//fdset에서 소켓을 해제함.
+						FD_CLR(i, &defaultFds);
+						close(i);
+						continue;
+					}
+					rBuff[readLen] = '\0';
+					printf("\nClient(%d): %s\n",i-3,rBuff);
+					write(i,rBuff, strlen(rBuff));
+				}
+			}
+		}
+	}
+	close(listenSd);
+	return 0;
+}
