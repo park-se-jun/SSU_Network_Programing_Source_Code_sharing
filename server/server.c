2c9d418d4f4ab655e303cdc2f348eac4a4f11112
NotableDeveloper <castlereal@naver.com>
2021-06-03 01:46:10 +0900

    서버를 멀티쓰레딩 환경으로 리팩토링

--- server/server.c
+++ server/server.c
@@ -4,109 +4,143 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <pthread.h>
 #include <unistd.h>
-#include <sys/time.h>
-#include <errno.h>
-#include <arpa/inet.h>
 #include <fcntl.h>
 
-int main(int argc, char** argv)
+void *client_connect(void *data);
+int init_source(int connectSd, int *write_fd, int *read_fd);
+int write_source(int connectSd, int *write_fd);
+int send_source(int connectSd, int *read_fd);
+
+/*
+	단일 프로세스가 하나의 클라이언트 요청을 받는 단점을 보완하기 위해,
+	여러 쓰레드로 여러 클라이언트의 요청을 받도록 한다.
+*/
+void *client_connect(void * data)
 {
+	int connectSd = *((int *) data);
+	int write_fd, read_fd;
+
+	while(1){
+		if(init_source(connectSd, &write_fd, &read_fd) == 0){
+			write(connectSd, "SUCESS", 7);
+			break;
+		}
+	}
+
+	while(1)	//클라이언트로부터 데이터를 읽어온다.
+	{
+		if(write_source(connectSd, &write_fd) == -1)
+			break;
+
+		if(send_source(connectSd, &write_fd) == -1)
+			break;
+	}
+
+	fprintf(stderr, "The client is disconnected.\n");
+	close(connectSd);
+}
+
+int init_source(int connectSd, int *write_fd, int *read_fd){
+	char source_name[BUFSIZ];
+	int name_length = read(connectSd, source_name, sizeof(source_name) - 1);
+
+	if(name_length <= 0){
+		fprintf(stderr, "Input error!\n");
+		write(connectSd, "FAIL", 5);
+		close(connectSd);
+		exit(0);
+	}
+
+	source_name[name_length] = '\0';
+	printf("Current working source code name is %s\n", source_name);
+	*write_fd = open(source_name, O_WRONLY|O_CREAT, 0666);
+
+	if(*write_fd == -1){
+		printf("File create error!\n");
+		write(connectSd, "FAIL", 5);
+		close(connectSd);
+		exit(1);
+	}
+
+	*read_fd = open(source_name, O_RDONLY, 0666);
+
+	if(*read_fd == -1){
+		printf("File create error!\n");
+		write(connectSd, "FAIL", 5);
+		close(connectSd);
+		exit(1);
+	}
+
+	return 0;
+}
+
+int write_source(int connectSd, int *write_fd){	//클라이언트로부터 읽어서 파일에 쓴다.
+	char rBuff[BUFSIZ];
+	int read_length = read(connectSd, rBuff, sizeof(rBuff) - 1);
+
+	if(read_length <= 0)
+		return -1;
+
+	//한 줄씩 개행이 들어가도록 맨 끝에 개행을 넣어줌.
+	rBuff[read_length] = '\n';
+	//rBuff[read_length + 1] = '\0';
+	printf("Client(%d): %s\n",connectSd, rBuff);
+
+	write(*write_fd, rBuff, strlen(rBuff));
+}
+
+int send_source(int connectSd, int *read_fd){	//클라이언트에게 파일의 내용을 전송한다.
+	/*
+	char wBuff[BUFSIZ];
+	int cur_pos = lseek(*read_fd, 0, SEEK_CUR);
+	lseek(*read_fd, 0, SEEK_SET);
+	read(*read_fd, wBuff, BUFSIZ);
+	printf("wBuff is %s\n", wBuff);
+	lseek(*read_fd, cur_pos, SEEK_SET);
+	*/
+}
+
+int main(int argc, char** argv){
 	int listenSd, connectSd;
 	struct sockaddr_in srvAddr, clntAddr;
-	int clntAddrLen, readLen, strLen;
-	char rBuff[BUFSIZ];
-	int maxFd = 0;
-	fd_set defaultFds, rFds;
-	int res, i;
-	int fd = open("test.c", O_RDWR|O_APPEND|O_CREAT, 0666);
+	int clntAddrLen, strLen;
+	pthread_t thread;
 
-	if(argc != 2)
-	{
+	if(argc != 2){
 		printf("Usage: %s [Port Number]\n", argv[0]);
 		return -1;
 	}
 
 	printf("Server start...\n");
-	listenSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	if(listenSd == -1 )
-		printf("socket error");
+	listenSd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	memset(&srvAddr, 0, sizeof(srvAddr));
-	//서버 addr 구조체 초기화.
 	srvAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	srvAddr.sin_family = AF_INET;
 	srvAddr.sin_port = htons(atoi(argv[1]));
 
-	//소켓과 addr 구조체 바인딩
-	if(bind(listenSd, (struct sockaddr *) &srvAddr, sizeof(srvAddr)) == -1){
-		printf("bind error\n");
-		return -1;
-	}
-
-	//5개의 연결 허용.
-	if(listen(listenSd, 5) < 0)
-		printf("listen error\n");
-
-	//fdset의 모든 비트열 초기화.
-	FD_ZERO(&defaultFds);
-	//listenSd 에 해당하는 비트열을 1로 설정.
-	FD_SET(listenSd, &defaultFds);
-	//select 사용을 위해, 생성된 디스크립터의 숫자를 따로 저장.
-	maxFd = listenSd;
+	bind(listenSd, (struct sockaddr *) &srvAddr, sizeof(srvAddr));
+	listen(listenSd, 5);
 
 	clntAddrLen = sizeof(clntAddr);
-	while(1)
-	{
-		rFds = defaultFds;	//select 호출 전에 fdset을 복사해둠.
 
-		//select으로 서버로 오는 이벤트를 감지.
-		if((res = select(maxFd + 1, &rFds, 0, 0, NULL)) == -1)
-			break;
+	while(1){	//accept이 될 때마다 쓰레드가 생성됨.
 
-		for(i=0; i<maxFd+1; i++)
-		{
-			if(FD_ISSET(i, &rFds))
-			{
-				if(i == listenSd) //accept a client
-				{
-					connectSd = accept(listenSd,
-						 (struct sockaddr *) &clntAddr,
-						 &clntAddrLen);
-					if(connectSd == -1)
-					{
-						printf("Accept Error");
-						continue;
-					}
-					printf("A client is connected...\n");
-					//연결된 소켓을 defaultFds에 등록해둠.
-					FD_SET(connectSd, &defaultFds);
+		connectSd = accept(listenSd,
+			   	(struct sockaddr *) &clntAddr, &clntAddrLen);
 
-					if(maxFd < connectSd){
-						maxFd = connectSd;
-					}
-				}
-				else //연결된 클라이언트의 요청
-				{
-					readLen = read(i, rBuff, sizeof(rBuff)-1);
-					if(readLen <= 0)
-					{
-						printf("A client is disconnected...\n");
-						//fdset에서 소켓을 해제함.
-						FD_CLR(i, &defaultFds);
-						close(i);
-						continue;
-					}
-					rBuff[readLen] = '\n';
-					rBuff[readLen + 1] = '\0';
-					//printf("\nClient(%d): %s\n", i-3, rBuff);
-					write(fd, rBuff, strlen(rBuff));
+		if(connectSd == -1)
+			continue;
 
-					write(i, rBuff, strlen(rBuff));
-				}
-			}
-		}
+		else
+			printf("A client is connected...\n");
+
+		//Thread를 생성하여 클라이언트의 요청을 처리하도록 함.
+		pthread_create(&thread, NULL, client_connect, (void *) &connectSd);
+		pthread_detach(thread);
 	}
 	close(listenSd);
 	return 0;
